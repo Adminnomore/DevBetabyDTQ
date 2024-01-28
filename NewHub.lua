9453253bfdba47853a998b5aaf8c493bd09a53c5
DTQ <156881790+Adminnomore@users.noreply.github.com>
2024-01-28 21:00:20 +0700

    Update NewHub.lua

--- NewHub.lua
+++ NewHub.lua
@@ -3,8 +3,8 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 --------------------------------------------------------------------------------------------------------------------------------------------
 local Window = Fluent:CreateWindow({
-    Title = "Beta Hub",
-    SubTitle = "by ĐTQ",
+    Title = "Beta Hub by TQuocZz",
+    SubTitle = "facebook.com/ily.tiwq",
     TabWidth = 160,
     Size = UDim2.fromOffset(450, 300),
     Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
